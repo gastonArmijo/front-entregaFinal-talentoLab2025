51432e9ff55e855adfe31470f3f45517ece0980a
Gaston <armijogaston@gmail.com>
2025-12-01 14:47:20 -0300

    actualizacion de fetch de productos

--- js/carusel.js
+++ js/carusel.js
@@ -1,5 +1,3 @@
-import { agregarAlCarrito } from "./funcionesCarrito.js";
-
 export const carousel = (carouselSelector, prevSelector, nextSelector, step = 220, visible = 4) => {
     const carousel = document.querySelector(carouselSelector);
     const prev = document.querySelector(prevSelector);
@@ -85,36 +83,3 @@ export const carousel = (carouselSelector, prevSelector, nextSelector, step = 22
 
 
 
-export const renderProductos = (productos, contenedorSelector) => {
-    const contenedor = document.querySelector(contenedorSelector);
-    if (!contenedor) return;
-
-    productos.forEach((p) => {
-        const tarjeta = document.createElement("article");
-        tarjeta.classList.add("card");
-
-        const img = document.createElement("img");
-        img.src = `./${p.img}`;
-        img.alt = p.nombre;
-
-        const titulo = document.createElement("p");
-        titulo.textContent = p.nombre;
-
-        const precio = document.createElement("h3");
-        precio.textContent = `$${p.precio}`;
-
-        const boton = document.createElement("button");
-        boton.classList.add("btn-carrito");
-        boton.textContent = "Agregar al carrito";
-
-        boton.addEventListener("click", () => {
-            agregarAlCarrito(p);
-        })
-
-        tarjeta.appendChild(img);
-        tarjeta.appendChild(titulo);
-        tarjeta.appendChild(precio);
-        tarjeta.appendChild(boton);
-        contenedor.appendChild(tarjeta);
-    });
-};
--- js/index.js
+++ js/index.js
@@ -1,26 +1,36 @@
-import { carousel, renderProductos} from "./carusel.js";
-import { productosArcanas, productosInmortales, productosTesoros } from "./productos.js";
+import { carousel} from "./carusel.js";
 import { menuHamburguesa } from "./menu.js";
-
 import { renderResenas } from "./resenas.js";
-
-import { agregarAlCarrito } from "./funcionesCarrito.js";
 import { obtenerCarrito } from "./storage.js";
 import { actualizarContador } from "./ui.js";
+import { renderProductos } from "./productos.js";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     menuHamburguesa();
-    renderProductos(productosArcanas, "#contenedor-arcanas")
-    renderProductos(productosInmortales, "#contenedor-inmortales");
-    renderProductos(productosTesoros, "#contenedor-tesoros");
+    
+    fetch("./data/productos.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`Error HTTP status;: ${res.status}`);
+            }
+            return res.json();
+        })
+            
+        .then((data) => {
+            // Renderizar cada categoría
+            renderProductos(data.productosArcanas, "#contenedor-arcanas");
+            renderProductos(data.productosInmortales, "#contenedor-inmortales");
+            renderProductos(data.productosTesoros, "#contenedor-tesoros");
 
-    // Inicializar carruseles
-    carousel("#contenedor-arcanas", ".prev-arcanas", ".next-arcanas");
-    carousel("#contenedor-inmortales", ".prev-inmortales", ".next-inmortales");
-    carousel("#contenedor-tesoros", ".prev-tesoros", ".next-tesoros");
+            // Inicializar carruseles DESPUÉS de renderizar
+            carousel("#contenedor-arcanas", ".prev-arcanas", ".next-arcanas");
+            carousel("#contenedor-inmortales", ".prev-inmortales", ".next-inmortales");
+            carousel("#contenedor-tesoros", ".prev-tesoros", ".next-tesoros");
+        })
+ 
+
 
     const carrito = obtenerCarrito();
     actualizarContador(carrito);
     renderResenas();
-
-})
+});
